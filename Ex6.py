268610795d517a3df39f6b9ab809b85ebb62960a
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 13:26:13 +0530

    Update Ex6.py

--- Ex6.py
+++ Ex6.py
@@ -1,3 +1,5 @@
+'''Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)'''
+
 input_string = input("Enter any word: ")
 
 if input_string == input_string[::-1]:
