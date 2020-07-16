6a59b62e3c9ffd01dc3b9595377f383f920f6ee7
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 13:05:59 +0530

    Update Ex1.py

--- Ex1.py
+++ Ex1.py
@@ -1,3 +1,5 @@
+'''Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.'''
+
 your_age = input("Enter your age: ")
 your_name = input("Enter your name: ")
 
