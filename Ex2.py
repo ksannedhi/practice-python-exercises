04539c90e898c93000c5c842952ea7dd1ae161a4
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 13:20:05 +0530

    Update Ex2.py

--- Ex2.py
+++ Ex2.py
@@ -1,3 +1,5 @@
+'''Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user.'''
+
 user_entry = int(input("Enter a number: "))
 
 if user_entry % 2 == 0:
