d8944c972b93a963666df2a2c7274b4e5427fbfd
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 13:14:10 +0530

    Update Ex13.py

--- Ex13.py
+++ Ex13.py
@@ -1,3 +1,8 @@
+'''Write a program that asks the user how many Fibonnaci numbers to generate and then generates them. Take this opportunity to think about how you can use functions.
+Make sure to ask the user to enter the number of numbers in the sequence to generate.
+(Hint: The Fibonnaci seqence is a sequence of numbers where the next number in the sequence is the sum of the previous two numbers in the sequence.
+The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, …)'''
+
 def fib(user_input):
     fib_series = []
     a = 1
@@ -14,3 +19,4 @@ def fib(user_input):
 
 user_input = int(input("Enter a number to generate the Fibonacci series: "))
 print(f"Fibonacci series for {user_input} is {fib(user_input)}")
+
