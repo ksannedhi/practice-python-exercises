66f19d6d8eabd3e19e5442655322abf4892e4dcf
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 13:11:53 +0530

    Update Ex12.py

--- Ex12.py
+++ Ex12.py
@@ -1,3 +1,6 @@
+'''Write a program that takes a list of numbers (for example, a = [5, 10, 15, 20, 25]) and makes a new list of only the first and last elements of the given list.
+For practice, write this code inside a function.'''
+
 def first_and_last(user_list):
     result = [user_list[0], user_list[-1]]
     return result
