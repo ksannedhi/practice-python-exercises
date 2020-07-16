618dc85285ddd17cbbecebdd85d74b9156a71948
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-16 13:10:41 +0530

    Update Ex11.py

--- Ex11.py
+++ Ex11.py
@@ -1,3 +1,6 @@
+'''Ask the user for a number and determine whether the number is prime or not. (For those who have forgotten, a prime number is a number that has no divisors.).
+You can (and should!) use your answer from Exercise 4 to help you. Take this opportunity to practice using functions.'''
+
 def find_prime_or_not(num):
     divisors = list(range(1,num+1))
     div_list = [div for div in divisors if num%div == 0]
