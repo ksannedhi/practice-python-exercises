fd91c82a82978c20e92afe553c0160edd5319de4
Kalyan Sannedhi <san.kalyan@gmail.com>
2020-07-23 14:21:22 +0530

    Update Ex9-1.py

--- Ex9-1.py
+++ Ex9-1.py
@@ -1,5 +1,7 @@
+'''Generate a random number between 1 and 9 (including 1 and 9). Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right.'''
+
 import random
-computer_guess = random.randint(1, 11)
+computer_guess = random.randint(1, 9)
 your_num = int(input("Guess a number: "))
 
 while True:
